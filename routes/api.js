f780cc8105af5441444838c6bd20d34b843eac34
Maros Karasek <maros.karasek@gmail.com>
2025-03-12 13:12:38 +0000

    changes

--- routes/api.js
+++ routes/api.js
@@ -8,17 +8,33 @@
 
 'use strict';
 
+const { json } = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
+
 module.exports = function (app) {
+
+  const books = [];
 
   app.route('/api/books')
     .get(function (req, res){
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      return res.json(books.map((book) => {
+        if(!book.commentcount) book.commentcount = 0;
+        return book;
+      }))
     })
     
     .post(function (req, res){
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
+
+      if(!title) return res.json('missing required field title')
+
+      const newBook = {_id: uuidv4(), title};
+      books.push(newBook)
+
+      return res.json(newBook)
     })
     
     .delete(function(req, res){
